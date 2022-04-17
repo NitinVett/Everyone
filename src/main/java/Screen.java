4a15e959e9d2f58232f2d97dd41fb4fa33d7a48b
Nitin <nitin.vettiankal@gmail.com>
2022-04-16 22:31:47 -0400

    2d array still not printing perfectly, fixed some bugs with content pane and jpanel.

--- src/main/java/Screen.java
+++ src/main/java/Screen.java
@@ -4,18 +4,18 @@ import java.awt.event.ActionListener;
 
 public class Screen extends JFrame{
     Map map = new Map();//object
-
+    JPanel panel = new JPanel();
     //starting screen
     public void setScreen()
     {
-        JPanel panel = new JPanel();
 
         this.setBounds(0,0,1300,820);
         this.setVisible(true);
         this.setTitle("28");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
-        this.getContentPane().setBackground(Color.GRAY);
+        panel.setBackground(Color.GRAY);
+        panel.setLayout(null);
        // this.getContentPane().setLayout(null);
         this.add(panel);
 
@@ -26,21 +26,15 @@ public class Screen extends JFrame{
     {
         JButton button = new JButton();
         button.setBounds(450,300,400,200);
-        JLabel background = new JLabel();
-        background.setBounds(200,200,1300,820);
         button.setFont(new Font("Courier", Font.BOLD, 50));
-        this.setLocationRelativeTo(null);
         button.setText("Play");
-        background.setVisible(true);
         button.setVisible(true);
-        this.add(button);
-        this.add(background);
+        panel.add(button);
         button.addActionListener(e -> {
-        this.getContentPane().removeAll();//removes everything
-        button.setVisible(false);
-        this.repaint();//refresh
+            this.getContentPane().removeAll();//removes everything
+            button.setVisible(false);
             drawMap();
-            validate();//gives the hint
+            validate();
         });
     }
 
@@ -49,9 +43,7 @@ public class Screen extends JFrame{
 
     public void drawMap()
     {
-        System.out.println("lol");
-       // this.remove(panel);
         this.add(map);
-        this.repaint();
+
     }
 }
